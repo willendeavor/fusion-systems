afeed7b39f012cc52101d5be3def373395cfee92
willendeavor <willhalford128@gmail.com>
2025-10-24 11:41:52 +0100

    Adding docs

---
+++ docs/general-group.md
@@ -0,0 +1,4 @@
+# general-group.m
+Contains all the general group functions that are used elsewhere in the code, for example there are functions to determine if a group contains a strongly $p$-embedded subgroup
+
+##
--- src/temporary-moving.m
+++ src/temporary-moving.m
@@ -73,103 +73,17 @@ declare attributes GrpPC: autogrp, autoperm, autopermmap, autF;
 
 
 ///////////////////
-intrinsic FocalSubgroupTest(B::Grp,S::Grp, Essentials::SeqEnum,AutF::Assoc)->
-Bool,Grp{Tests if S eq Focal subgroup in potential system};
-Foc:=sub<S|{(s,b):s in Generators(S),  b in Generators(B)}>;
-for e in Essentials do  
-Foc:= sub<S|Foc, {x^-1*aa(x): x in e, aa in Generators(AutF[e])}>;
-end for;
-return Foc eq S, Foc;
-end intrinsic;
 
-///////////////////////////////
-intrinsic AutFCore(Es::SeqEnum,AutE::SeqEnum)->Grp,GrpAuto
-{calculates F core and action on it}
-F:= Es[1];
-for i := 2 to #Es do  F:= F meet Es[i];end for; 
- Fn:= sub<F|>;
- while F ne Fn do
- Fn:= F;
- for i := 1 to #Es do
-    A:= AutOrbit(Es[i],F,AutE[i]);
-    for x in A do F := F meet x; end for;
- end for;
- end while;
-MakeAutos(F); K:= Inn(F); 
-    for i := 1 to #Es do  K:=  sub<F`autogrp|K,Generators(AutE[i])>; end for;
-    return F,K;
-end intrinsic;
+
+
 //////////////////////////////
-intrinsic RadicalTest(S::Grp,P::Grp)->Boolean{Checks if P could be a radical subgroup}
- A:=AutYX(Normalizer(S,P),P);
-	Ap:= SubMap(P`autopermmap,P`autoperm ,A);
-		Inner:= Inn(P);
-	Innerp:= SubMap(P`autopermmap,P`autoperm ,Inner);
-RadTest:=#(Ap meet pCore(P`autoperm, FactoredOrder(S)[1][1])) eq  #Innerp;
-return RadTest;
-end intrinsic;
+
 //////////////////////////////
-intrinsic CentreTest(B::Grp,S::Grp, Essentials::SeqEnum,AutF::Assoc)->
-Bool{Tests is action on Z(S) by B and that induced by memebers of AutF is consistent};
-Z:= Centre(S);
-ZB:=  AutYX(B,Z);
-ZA:= sub<Z`autogrp|>;
-for e in Essentials do 
-Orb, NN:=  AutOrbit(e,Z,AutF[e]);
-ZA := sub<Z`autogrp|ZA,Generators(NN)>;
-end for;
-TT:={x in ZB: x in Generators(ZA)};
-return TT eq {true};
-end intrinsic;
+
 
 ///////////////////////////////
 
-intrinsic CentreTest(B::Grp,S::Grp,P::Grp,A::GrpAuto)->
-Bool{Tests is action on Z(S) by B and that induced by members of A is consistent};
 
-ZS:= Centre(S);
-SubZ:= Subgroups(ZS);
-for zz in SubZ do 
-    Z:= zz`subgroup; if #Z eq 1 then continue; end if;
-    ZB:=  AutYX(Normalizer(B,Z),Z);
-    Orb, NN:=  AutOrbit(P,Z,A);
-    ZA:= sub<Z`autogrp|Generators(NN)>;
-    TT:={x in ZB: x in Generators(ZA)};
-    if TT ne {true} then return false; end if; 
-end for;
-
-return true;
-end intrinsic;
-
-///////////////////////////////
-
-intrinsic FCoreTest(S::Grp,Essentials::SeqEnum, AutF::Assoc)->Bool, Grp
-{Test is F core is trivial and returns FCore}
-F:= S; FC:= sub<S|>; AA:=Append(Essentials,S);
-for x in Essentials do F := F meet x; end for;
-SF:=[x`subgroup:x in  NormalSubgroups(F)];
-SF:= Reverse(SF);
-
-  
-
-for x in SF do 
- if IsNormal(S,x) eq false then continue x; end if;
-    for E in AA do
-        for y in Generators (AutF[E]) do
-            for xx in Generators(x) do
-                if y(xx) in x eq false   then 
-                continue x; end if;
-            end for;
-        end for;
-    end for;
-    FC:= sub<S|x>; break;
- end for;
- 
- 
-return #FC eq 1, FC;
-
-end intrinsic;
- 
 
 /////////////////////////////////
 /////////////AutFPCandidates.
@@ -186,18 +100,6 @@ end intrinsic;
 
 
 
-intrinsic Print(F::FusionSystem)
-{Print F}
-
-E:= [(#F`essentialautos[i]*#Centre(F`essentials[i]))/#F`essentials[i]:i in [2.. #F`essentialautos]];
-E1:= [#F`essentials[i]:i in [2.. #F`essentials]];
-printf "Fusion System with %o", #F`essentials-1; printf "\ F-classes of essential subgroups", "\n"; 
-printf "\nThey have orders: %o", E1, "\n"; 
-printf "\Out_F(E)  have orders: %o", E; printf 
-"\nOut_\F(S) has order  %o", #F`essentialautos[1]/#Inn(F`group);
-if assigned(F`grpsystem) then
-printf "\nThis is a group fusion system\n"; end if;
-end intrinsic;
 
 //////////////////////////////////////////////////////////////////////////
  //Given a subgroup Q and a morphism Q -> S in F, calculate NPhi. 
@@ -246,40 +148,14 @@ end intrinsic;
 ///P is in SS
 ///////////////////////////////
 
-  intrinsic IdentifyFOrbit(F::FusionSystem,P::Grp)->SetEnum
-  {identifies the connected component of the fusion graph corresponding to P}    
-      if assigned(F`fusiongraph) eq false then 
-          F`fusiongraph, F`maps:= FusionGraph(F); 
-      end if;   F`classes:= ConnectedComponents(F`fusiongraph);
-      ii:= Index(F`subgroups,P);
-                for C in F`classes do
-                    if ii in C then D:= C; break; end if;
-                end for;
-      return D;
-      end intrinsic;
+  
 
 
 //////////////////////////////////////////
 ////Suppose that P \le S. Determine the F-class of P
 /////////////////////////////
 
-intrinsic ConjugacyClass(F::FusionSystem,P:Grp)-> SetEnum
-{Determines the F-conjugacy class of a subgroup}
-SS:= F`subgroups;S:= F`group;
-    if assigned(F`fusiongraph) eq false then 
-        F`fusiongraph,F`maps:= FusionGraph(F);  
-        
-    end if; F`classes:= ConnectedComponents(F`fusiongraph);
 
-ii:= IdentifyBClass(F,P);
-
-Clss:= IdentifyFOrbit(F,SS[ii]);  
-FCls:={};
-for w in Clss do 
-    FCls:= FCls join{ SS[Index(w)]^x:x in F`borel}; 
-end for;
-return FCls;
-end intrinsic;
 
 
    //////////////////////////////////////////
@@ -287,189 +163,44 @@ end intrinsic;
 ///Returns true or false and if true a map in F
 /////////////////////////////
 
-intrinsic IsConjugate(F::FusionSystem, P::Grp,Q::Grp)->Bool,Map 
-{returns a homomorphism and true if the groups are F-conjugate}
 
-SS:= F`subgroups; S:= F`group; B:= F`borel; 
-if assigned(F`fusiongraph) eq false then 
-    F`fusiongraph,F`maps:= FusionGraph(F);  
-    
-end if;
-F`classes:= ConnectedComponents(F`fusiongraph); 
-Maps:= F`maps; 
-V:= Vertices(F`fusiongraph);
-
-conj := false; gg:= Id(S);
-    if #P ne #Q then return false, _; end if;
-   
-   
-ii, b1:= IdentifyBClass(F,P);
-jj, b2:= IdentifyBClass(F,Q);
-
-            if Reachable(V!ii, V!jj) then
-               gg:=  ConjtoHom(P,SS[ii],b1); 
-               hh:=ConjtoHom(SS[jj],Q,b2^-1);
-               Geo:=Geodesic(V!ii,V!jj);
-               if #Geo eq 0 then conj eq false;
-                    else conj := true;
-                    for iii:= 1 to #Geo-1 do 
-                        gg:= gg*Maps[[Index(Geo[iii]),Index(Geo[iii+1])]]; 
-                    end for;
-              end if;
-              gg:=gg*hh;
-          end if;
-return conj,gg;
-end intrinsic;
-
-///////////////////////////////////////////////////////////////////////////
-/////////IsConjugate for elements.
-///////////////////////////////////////////////////////////////////////
-intrinsic IsConjugate(F::FusionSystem, g::GrpElt,h::GrpElt)->Bool,Map 
-{returns a homomorphism and true if the elements are F-conjugate}
-S:= F`group;
-B:= F`borel;
-
-a,b := IsConjugate(B,g,h);
-if a then b :=  ConjtoHom(B, B, b); return true,b;end if;
-
-
-if assigned(F`fusiongraph) eq false then 
-    F`fusiongraph,F`maps:= FusionGraph(F); 
-end if;
-
-G:= sub<B|g>; H:= sub<B|h>;
-a,b:=IsConjugate(F , G,H);
-if a eq false then return false; end if; 
-g1:= b(g);
- 
-a1, bb:= IdentifyBClass(F,H);
-
-P:= H^bb;
-
-APH:= AutomorphismGroup(F,P);
-g2 := g1^bb;
-h1 := h^bb;
-
-CC, JJ, KK:= AutOrbit(P, g2, APH);
-if h1 in CC eq false then return false; 
-    else k:= Index(CC,h1); mp:= KK[k];
-    map := b*ConjtoHom(S,S,bb)*mp*ConjtoHom(S,S,bb^-1); return true, map;
-    end if;
-    
-return false,_;
-end intrinsic;
 
 ///////////////////////
 /////// F-conjugacy class
 ///////////////////////////////
-intrinsic FConjugacyClass(F::FusionSystem,x::GrpElt)->  Set
-{Returns set of all F-conjugates of x}
-B:= F`borel;
-BC:= ConjugacyClasses(B);
-CC:={};
-for t in BC do
-    s:= t[3]; 
-    if Order (s) ne Order (x) then continue; end if;
-    a:= IsConjugate(F,x,s); 
-    if a then CC:= CC join Class(B,s); end if;
-end for;
-return CC;
-end intrinsic;
+
 
 /////////////////////////
 // SizeFConjugacyClass
 /////////////////////////
-intrinsic  SizeFConjugacyClass(F::FusionSystem,x::GrpElt)->  RngElt
-{Returns size of the F-class of x}
- return #FConjugacyClass(F,x);
-end intrinsic;
+
 
 /////////////////////////////////////
 /// FConjugacyClasses
 ///////////////////////////////////
 
 
-intrinsic  FConjugacyClasses(F)-> Set
-{Returns FConjugacyClasses}
- B:= F`borel;
- BC:= ConjugacyClasses(B);
- CCC:={};kk:=0;
- for t in BC do
-    s:= t[3]; for cc in CCC do if s in cc then continue; end if; end for;
-    if s in F`group eq false then continue; end if;
-    k:= #FConjugacyClass(F,s); kk:= kk+k;
-    CCC:= CCC join {FConjugacyClass(F,s)};
-    if kk eq #F`group then break; end if;
- end for;
- 
-return CCC; 
-end intrinsic;
+
 
 /////////////////////////////////////
 ///Number  FConjugacyClasses
 ///////////////////////////////////
 
 
-intrinsic  SizeFConjugacyClasses(F)-> RngElt
-{Returns Size of  FConjugacyClasses}
-return #FConjugacyClasses(F);
-end intrinsic;
 
 
 ///////////////////////////////////
 ///////IsCentric(P).
 ///////////////////////////////////
 
-intrinsic IsCentric(F::FusionSystem,P::Grp)->Bool
-{Returns true if P is F-centric}
 
-SS:= F`subgroups; S:= F`group; B:= F`borel; 
-if assigned(F`fusiongraph) eq false then 
-    F`fusiongraph,F`maps:= FusionGraph(F); 
-end if;
-  F`classes:= ConnectedComponents(F`fusiongraph); 
-Cent:= true;
-
-ii:= IdentifyBClass(F,P);
-P:= F`subgroups[ii];
-C:=  IdentifyFOrbit(F,P);
-     for jj in C do
-        if Centre(SS[Index(jj)]) ne Centralizer(S,SS[Index(jj)]) 
-            then return false;
-        end if;
-     end for;
- return Cent;
- end intrinsic;
 
 
 ///////////////////////////////////
 ///////IsFullyNormalized(P).
 ///////////////////////////////////
 
-intrinsic IsFullyNormalized(F::FusionSystem, P::Grp)->Bool{Is the subgroup full F-Normalized}
 
-SS:= F`subgroups; S:= F`group; B:= F`borel; 
-if assigned(F`fusiongraph) eq false then 
-F`fusiongraph,F`maps:= FusionGraph(F);  
-end if;
- F`classes:= ConnectedComponents(F`fusiongraph); 
- 
-FN:= true;
-
-ii:= IdentifyBClass(F,P);
-P:= F`subgroups[ii];
-
-n:=#Normalizer(S,P);
-C:=IdentifyFOrbit(F,P);
-for jj in C do 
-    m:= #Normalizer(S,SS[Index(jj)]);    
-    if m gt n then FN:= false;
-        break jj; 
-    end if; 
-end for; 
-
-return FN;
-end intrinsic;
 
 
 /////////////////////////////////////
@@ -477,27 +208,6 @@ end intrinsic;
 /////////////////////////////////////
 
 
-intrinsic IsFullyCentralized(F::FusionSystem, P::Grp)->Bool{Is the subgroup full F-Centralized}
-SS:= F`subgroups; S:= F`group; B:= F`borel; 
-if assigned(F`fusiongraph) eq false then F`fusiongraph,F`maps:=FusionGraph(F);  
-end if;
- F`classes:= ConnectedComponents(F`fusiongraph); 
-
-    FC:= true;
-    n:=#Centralizer(S,P);
-
-ii:= IdentifyBClass(F,P);
-P:= F`subgroups[ii];
-
-C:=IdentifyFOrbit(F,P);
-for jj in C do 
-    m:= #Centralizer(S,SS[Index(jj)]);    
-    if m gt n then FC:= false;
-        break jj; 
-    end if; 
-end for; 
-return FC;
-end intrinsic;
 
 
 //////////////////////////////////////////////////////////////////////
