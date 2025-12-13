1dd6336c96535a22e2d126e7002f678fab1669c9
willendeavor <willhalford128@gmail.com>
2025-12-13 16:31:37 +0000

    Hyperfocal and residual tests
    Both are being failed currently

--- tests/test-is-residual.m
+++ tests/test-is-residual.m
@@ -1,4 +1,10 @@
 // Testing IsResidual
 flag := true;
 // Residual example: F_S(PSU(4,p))
-F := GroupFusionSystem(F)
+F := GroupFusionSystem(PSU(4,5),5);
+if IsResidual(F) eq false then
+	flag := false;
+	print "Test failed: PSU(4,5)";
+end if;
+
+// Non-residual example: 
